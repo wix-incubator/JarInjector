645e670209971990572fb5e839e8665c8236ab8b
Evgeni Silman <evgenis@wix.com>
2021-08-04 17:36:47 +0300

    Initial commit

---
+++ src/main/java/com/evgenis/injector/JarModifier.java
@@ -0,0 +1,73 @@
+package com.evgenis.injector;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.jar.JarOutputStream;
+
+public class JarModifier {
+    private static class Entry {
+        Entry(String name, byte[] bytes) {
+            this.name = name;
+            this.bytes = bytes;
+        }
+
+        String name;
+        byte[] bytes;
+    }
+
+    private final JarFile jarFile;
+    private final List<String> entriesToDelete = new ArrayList<>();
+    private final List<Entry> entriesToPut = new ArrayList<>();
+
+    public JarModifier(String path) throws IOException {
+        jarFile = new JarFile(path);
+    }
+
+    public byte[] readEntry(String name) throws IOException {
+        JarEntry jarEntry = jarFile.getJarEntry(name);
+        InputStream inputStream = jarFile.getInputStream(jarEntry);
+        byte[] bytes = new byte[inputStream.available()];
+        inputStream.read(bytes);
+        inputStream.close();
+        return bytes;
+    }
+
+    public JarModifier deleteEntry(String name) {
+        entriesToDelete.add(name);
+        return this;
+    }
+
+    public JarModifier putEntry(String name, byte[] bytes) {
+        entriesToDelete.add(name);
+        entriesToPut.add(new Entry(name, bytes));
+        return this;
+    }
+
+    public void build(String path) throws IOException {
+        JarOutputStream jarOutputStream = new JarOutputStream(new FileOutputStream(path));
+
+        Enumeration<JarEntry> jarEntryEnumeration = jarFile.entries();
+        while (jarEntryEnumeration.hasMoreElements()) {
+            final JarEntry jarEntry = jarEntryEnumeration.nextElement();
+            if (!entriesToDelete.contains(jarEntry.getName())) {
+                jarOutputStream.putNextEntry(new JarEntry(jarEntry.getName()));
+                jarOutputStream.write(readEntry(jarEntry.getName()));
+                jarOutputStream.closeEntry();
+            }
+        }
+
+        for (Entry entry : entriesToPut) {
+            jarOutputStream.putNextEntry(new JarEntry(entry.name));
+            jarOutputStream.write(entry.bytes);
+            jarOutputStream.closeEntry();
+        }
+
+        jarOutputStream.close();
+    }
+}
---
+++ src/main/java/com/evgenis/injector/Main.java
@@ -0,0 +1,22 @@
+package com.evgenis.injector;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.out.println("Usage: injector jarFile javaFile1 [JavaFile2 ...]");
+            return;
+        }
+
+        String jarFile = args[0];
+        String[] javaSources = new String[args.length - 1];
+        System.arraycopy(args, 1, javaSources, 0, args.length - 1);
+        Injector injector = new Injector(jarFile, javaSources);
+        try {
+            injector.inject();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
